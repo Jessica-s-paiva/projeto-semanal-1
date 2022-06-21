aca77ba6795b76f7347b7f0ac58a690d5de78a25
Jessica-s-paiva <jessicapaivamvp@gmail.com>
2022-06-20 22:47:36 -0300

    retornando string ordenada

--- index.js
+++ index.js
@@ -19,15 +19,16 @@ var readline = require('readline');
 var rl = readline.createInterface(
         process.stdin, process.stdout);
   
-rl.setPrompt(`What is your age? `);
+rl.setPrompt(`Quais comandos deseja listar? \n`);
 rl.prompt();
 let array = [];
-rl.on('line', (age) => {
-    if (age == 'SAIR'){//we need some base case, for recursion
-    console.log(array.sort());
+rl.on('line', (comand) => {
+    if (comand == 'SAIR'){//we need some base case, for recursion
+        const arrayOrd = array.sort();
+    console.log(arrayOrd.join(" "));
     return rl.close();
     }
-    console.log(`Age received by the user: ${age}`);
-    array.push(age)
+    //console.log(comand);
+    array.push(comand)
     //rl.close();
 });
